0e3d1b481a1bc5990d46ff44da76533fe91accdb
Blixibon <acj3017@gmail.com>
2021-03-21 02:06:19 -0500

    Added criterion parsing, dividers, more concepts, etc.

--- QuickRR.py
+++ QuickRR.py
@@ -4,9 +4,14 @@
 # 
 
 input_file = open("input_responses.csv")
-data = dict()
+ruleData = dict()
+criterionData = dict()
 
 lineNumber = 0
+
+# Script Information
+Mode = "Response"
+DividerNumber = 0
 
 # Columns
 Column_Response = 0
@@ -20,6 +25,11 @@ Column_ResponseParams = 7
 Column_RuleParams = 8
 Column_GroupParams = 9
 
+Column_Criterion = 0
+Column_CriterionKey = 1
+Column_CriterionValue = 2
+Column_CriterionParams = 3
+
 # Actor Criteria
 ActorCriteria = dict()
 ActorCriteria["Bad Cop"] = "IsBadCop"
@@ -28,7 +38,26 @@ ActorCriteria["Will-E"] = "IsWilson"
 
 # Concept Criteria
 ConceptCriteria = dict()
-ConceptCriteria["TLK_TIPPED"] = "ConceptTalkTipped"
+ConceptCriteria["TLK_TIPPED"] = "ConceptWilsonTipped"
+ConceptCriteria["TLK_FIDGET"] = "ConceptWilsonFidget"
+ConceptCriteria["TLK_FOUNDPLAYER"] = "ConceptFoundPlayer"
+ConceptCriteria["TLK_REMIND_PLAYER"] = "ConceptRemindPlayer"
+ConceptCriteria["TLK_APC_LOW_CLEARANCE"] = "ConceptAPCLowClearance"
+ConceptCriteria["TLK_APC_EJECTED"] = "ConceptAPCEjected"
+ConceptCriteria["TLK_GOODBYE"] = "ConceptWilsonGoodbye"
+ConceptCriteria["TLK_BEAST_DANGER"] = "ConceptWilsonBeastDanger"
+ConceptCriteria["TLK_BOSS_FIGHT"] = "ConceptWilsonBossFight"
+ConceptCriteria["TLK_WITNESS_EAT"] = "ConceptWitnessEat"
+ConceptCriteria["TLK_PLDEAD"] = "ConceptPlayerDead"
+ConceptCriteria["TLK_PLHURT"] = "ConceptTalkPlayerHurt"
+ConceptCriteria["TLK_PLRELOAD"] = "ConceptPlayerReload"
+ConceptCriteria["TLK_WOUND"] = "ConceptTalkWound"
+ConceptCriteria["TLK_WATCHOUT"] = "ConceptTalkWatchOut"
+ConceptCriteria["TLK_ALLY_IN_BARNACLE"] = "ConceptAllyInBarnacle"
+ConceptCriteria["TLK_DANGER"] = "ConceptTalkDanger"
+ConceptCriteria["TLK_STARTCOMBAT"] = "ConceptStartCombat"
+ConceptCriteria["TLK_ENEMY_DEAD"] = "ConceptEnemyDead"
+ConceptCriteria["TLK_USE"] = "ConceptTalkUse"
 
 # Response Group/Rule Lines
 Line_Response = ""
@@ -41,58 +70,89 @@ Line_GroupParams = ""
 for line in input_file:
     parts = line.split('^')
     
-    # Skip irrelevant rows!
+    IsHeader = False
+    if parts[Column_Response] == 'Response' or parts[Column_Response] == 'Criterion':
+        IsHeader = True
+        Mode = parts[Column_Response]
+    
+    # Some rows aren't responses!
     # 1. Responses called "Response" are headers describing the columns and not actual responses
-    # 2. Rows with blank line text are considered to be non-responses
-    if parts[Column_Response] == 'Response' or parts[Column_LineText] == "":
-        continue
+    # 2. Rows with blank line text AND blank entries are considered to be non-responses
+    if IsHeader or (parts[Column_LineText] == "" and parts[Column_Entry] == ""):
+    
+        # Instead, assume irrelevant rows are appropriate break points
+        Divider = "//========================="
+        if not IsHeader and parts[Column_Response].strip() != "":
+            Divider += " " + parts[Column_Response] + " "
+        Divider += "=========================\n\n"
         
-    # If the first character is a $, it's a cut line
-    if parts[Column_LineText][0] == '$':
+        if Mode == "Response":
+            ruleData["divider_" + str(DividerNumber)] = Divider
+        elif Mode == "Criterion":
+            criterionData["divider_" + str(DividerNumber)] = Divider
+        
+        DividerNumber += 1
         continue
     
     lineNumber = lineNumber + 1
         
-    # If this is a new response (i.e. not one carried over from a previous row), refresh our values
-    # (Retaining this data between similar rows is important for merged cells!!!)
-    if parts[Column_Response] != "":
-        Line_Response = parts[Column_Response]
-        Line_Actor = parts[Column_Actor]
-        Line_Concept = parts[Column_Concept]
-        Line_Criteria = parts[Column_Criteria]
-        Line_RuleParams = parts[Column_RuleParams]
-        Line_GroupParams = parts[Column_GroupParams]
-        
-    Line_LineText = parts[Column_LineText]
-    Line_LineNotes = parts[Column_LineNotes]
-    Line_Entry = parts[Column_Entry]
-    Line_ResponseParams = parts[Column_ResponseParams]
+    if Mode == "Response":
     
-    # Check if the response exists already
-    rule = data.get(Line_Response)
-    if rule is None:
-        lineNumber = 0
-        rule = dict()
+        # If this is a new response (i.e. not one carried over from a previous row), refresh our values
+        # (Retaining this data between similar rows is important for merged cells!!!)
+        if parts[Column_Response] != "":
+            Line_Response = parts[Column_Response]
+            Line_Actor = parts[Column_Actor]
+            Line_Concept = parts[Column_Concept]
+            Line_Criteria = parts[Column_Criteria]
+            Line_RuleParams = parts[Column_RuleParams]
+            Line_GroupParams = parts[Column_GroupParams]
+            
+        Line_LineText = parts[Column_LineText]
+        Line_LineNotes = parts[Column_LineNotes]
+        Line_Entry = parts[Column_Entry]
+        Line_ResponseParams = parts[Column_ResponseParams]
         
-    # Append criteria if it doesn't have any
-    criteria = rule.get("_criteria")
-    if criteria is None:
-        criteria = [Line_Actor, Line_Concept, Line_Criteria]
+        # Check if the response exists already
+        rule = ruleData.get(Line_Response)
+        if rule is None:
+            lineNumber = 0
+            rule = dict()
+            
+        # Append criteria if it doesn't have any
+        criteria = rule.get("_criteria")
+        if criteria is None:
+            criteria = [Line_Actor, Line_Concept, Line_Criteria]
+            
+        # Append rule params if it doesn't have any
+        ruleParams = rule.get("_ruleparams")
+        if ruleParams is None:
+            ruleParams = [Line_RuleParams, Line_GroupParams]
+            
+        # Check for this particular response entry
+        response = rule.get(lineNumber)
+        if response is None:
+            response = [Line_Entry, Line_ResponseParams, Line_LineText, Line_LineNotes]
         
-    # Append rule params if it doesn't have any
-    ruleParams = rule.get("_ruleparams")
-    if ruleParams is None:
-        ruleParams = [Line_RuleParams, Line_GroupParams]
+        rule["_criteria"] = criteria
+        rule["_ruleparams"] = ruleParams
+        rule[lineNumber] = response
+        ruleData[Line_Response] = rule
         
-    # Check for this particular response entry
-    response = rule.get(lineNumber)
-    if response is None:
-        response = [Line_Entry, Line_ResponseParams, Line_LineText]
+    elif Mode == "Criterion":
     
-    rule["_criteria"] = criteria
-    rule["_ruleparams"] = ruleParams
-    rule[lineNumber] = response
-    data[Line_Response] = rule
+        Line_Criterion = parts[Column_Criterion]
+        Line_CriterionKey = parts[Column_CriterionKey]
+        Line_CriterionValue = parts[Column_CriterionValue]
+        Line_CriterionParams = parts[Column_CriterionParams]
+        
+        # Check if the criterion exists already
+        criterion = ruleData.get(Line_Criterion)
+        if criterion is None:
+            lineNumber = 0
+            criterion = [Line_CriterionKey, Line_CriterionValue, Line_CriterionParams]
+        
+        criterionData[Line_Criterion] = criterion
 
 # Export responses
 responses_file = open("output_responses.txt", "w")
@@ -101,8 +161,35 @@ responses_file.write("//=============================================\n")
 responses_file.write("// AUTO-GENERATED RESPONSES\n")
 responses_file.write("//=============================================\n\n")
 
-for Rule_Name in data.keys():
-    rule = data.get(Rule_Name)
+# 
+# CRITERIA DECLARATIONS
+# 
+for Criterion_Name in criterionData.keys():
+    # If it's a divider, divide
+    if "divider_" in Criterion_Name:
+        responses_file.write(criterionData.get(Criterion_Name))
+        continue
+
+    criterion = criterionData.get(Criterion_Name)
+    
+    Criterion_Key = criterion[0]
+    Criterion_Value = criterion[1]
+    Criterion_Params = criterion[2]
+                
+    responses_file.write("criterion \"" + Criterion_Name + "\" \"" + Criterion_Key + "\" \"" + Criterion_Value + "\" " + Criterion_Params + "\n")
+    
+    #responses_file.write("//--------------------------------\n\n")
+
+# 
+# RESPONSE DECLARATIONS
+# 
+for Rule_Name in ruleData.keys():
+    # If it's a divider, divide
+    if "divider_" in Rule_Name:
+        responses_file.write(ruleData.get(Rule_Name))
+        continue
+
+    rule = ruleData.get(Rule_Name)
     
     # Declare text that will eventually be in the response
     Text_Criteria = ""
@@ -149,20 +236,39 @@ for Rule_Name in data.keys():
         Response_Entry = response[0]
         Response_ResponseParams = response[1]
         Response_LineText = response[2]
+        Response_LineNotes = response[3]
         
-        # If the entry is empty, assume placeholder text
+        Text_ResponseGroup += "\t"
+        
+        if Response_LineText != "" and Response_LineText[0] == '$':
+            # Response is commented out
+            Response_LineText = Response_LineText[1:]
+            Text_ResponseGroup += "//"
+        
         if Response_Entry == "":
-            Text_ResponseGroup += "\tprint \"" + Response_LineText + "\" " + Response_ResponseParams + "\n"
+            # If the entry is empty, assume placeholder text
+            Text_ResponseGroup += "print \"" + Response_LineText + "\" " + Response_ResponseParams
+            if Response_LineNotes != "":
+                Text_ResponseGroup += " // " + Response_LineNotes
+            Text_ResponseGroup += "\n"
         else:
             # Deduce entry type
             Response_Type = ""
             if ".vcd" in Response_Entry:
                 Response_Type = "scene"
+            elif "response " in Response_Entry:
+                Response_Type = "response"
+                Response_Entry = Response_Entry[9:].replace('\"', '')
             else:
                 # Assume speak for now
                 Response_Type = "speak"
             
-            Text_ResponseGroup += "\t" + Response_Type + " \"" + Response_Entry + "\" " + Response_ResponseParams + " // " + Response_LineText + "\n"
+            Text_ResponseGroup += Response_Type + " \"" + Response_Entry + "\" " + Response_ResponseParams + " //"
+            if Response_LineText != "":
+                Text_ResponseGroup += " \"" + Response_LineText + "\""
+            if Response_LineNotes != "":
+                Text_ResponseGroup += " -- " + Response_LineNotes
+            Text_ResponseGroup += "\n"
                 
     responses_file.write("response \"" + Rule_Name + "\"\n")
     responses_file.write("{\n")
@@ -179,8 +285,9 @@ for Rule_Name in data.keys():
     responses_file.write("\tresponse\t\t" + Rule_Name + "\n")
     responses_file.write("}\n\n")
     
-    responses_file.write("//--------------------------------\n\n")
+    #responses_file.write("//--------------------------------\n\n")
 
 responses_file.close()
 
-print(data)
+print(criterionData)
+print(ruleData)
